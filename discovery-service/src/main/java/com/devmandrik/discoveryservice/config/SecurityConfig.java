d4dc045439e7166e995b71796aed65a9bd62d146
Artsiom Mandryk <artik.mandrik@gmail.com>
2023-08-26 12:36:35 +0300

    Added security

---
+++ discovery-service/src/main/java/com/devmandrik/discoveryservice/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.devmandrik.discoveryservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig  {
+
+    @Value("${app.eureka.username}")
+    private String username;
+    @Value("${app.eureka.password}")
+    private String password;
+
+    @Bean
+    public InMemoryUserDetailsManager configure() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests((authz) -> authz
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
